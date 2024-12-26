aeddb31605f97ea839e092c6f9bc38d6a4807fe2
Lenni <lennard.kru@gmail.com>
2024-12-26 17:35:24 +0100

    added some stuff

--- src/main/java/net/lennilid/tutorial/Tutorial.java
+++ src/main/java/net/lennilid/tutorial/Tutorial.java
@@ -2,6 +2,7 @@ package net.lennilid.tutorial;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.lennilid.tutorial.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,7 @@ public class Tutorial implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 
 
 		LOGGER.info("Hello Fabric world!");
