2d39a2b2c1d8f1241776b701ecdb419c73cecbe9
Lenni <lennard.kru@gmail.com>
2024-12-26 16:42:54 +0100

    Initial commit

---
+++ src/main/java/net/lennilid/tutorial/TutorialDataGenerator.java
@@ -0,0 +1,11 @@
+package net.lennilid.tutorial;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TutorialDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
