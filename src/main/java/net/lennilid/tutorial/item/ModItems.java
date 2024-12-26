08fc252892fcd7d14a3966e222a05d68dd605f2f
Lenni <lennard.kru@gmail.com>
2024-12-26 20:08:17 +0100

    added some stuff

--- src/main/java/net/lennilid/tutorial/item/ModItems.java
+++ src/main/java/net/lennilid/tutorial/item/ModItems.java
@@ -4,12 +4,15 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.lennilid.tutorial.Tutorial;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static final Item OrangeFruit = registerItem("orange_fruit", new Item(new Item.Settings()));
+    public static final Item Banana = registerItem("banana", new Item(new Item.Settings()));
+
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(Tutorial.MOD_ID, name), item);
@@ -18,8 +21,9 @@ public class ModItems {
     public static void registerModItems() {
         Tutorial.LOGGER.info("Registering Mod Items for " + Tutorial.MOD_ID);
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroup.INGREDIENTS).register(entries -> {
-            entries.add(OrangeFruit);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(content -> {
+            content.add(OrangeFruit);
+            content.add(Banana);
         });
     }
 }
